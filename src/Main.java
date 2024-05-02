933ac45504fe95bbe202acf77cf7944fa1aad585
Jose Reyes <reyreyes2@gmail.com>
2024-05-02 00:05:48 -0500

    validaciones

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,31 +14,52 @@ public class Main {
         String sinopsis = """
                 Matrix, la mejor pelicula del fin del milenio
                 """;
-        double mediaEvalucionUsuario = 0;
 
-        System.out.println("Pelicula: "+nombre);
+
+        System.out.println("Pelicula: " + nombre);
         System.out.println(fechaDeLanzamiento);
         System.out.println(evaluacion);
         System.out.println(incluidoEnElPlanBasico);
 
         double mediaEvalucacion = (4.5 + 4.8 +3) /3;
-        System.out.println("Media de la Evalución de Matrix: "+mediaEvalucacion);
+        System.out.println("Media de la Evalución de Matrix: " + mediaEvalucacion);
 
         if (fechaDeLanzamiento >= 2023) {
             System.out.println("Pelicula Popular en el momento");
         }else{
-            System.out.println("Pelicula retro que vale la pena ver");
+            System.out.println("Pelicula retro que vale la pena ver \n");
         }
 
 
         //evaluacion de la pelicula forrest Gump
+
+        double mediaEvalucionUsuario = 0;
+
         System.out.println("Ahora vamos a evaluar la pelicula Forrest Gump");
         for (int i = 0; i < 3; i++) {
-            Scanner teclado = new Scanner(System.in);
-            System.out.println("Ingrese nota que le darias a Forrest Gump: ");
-            double notaForrestGump = teclado.nextDouble();
-            mediaEvalucionUsuario = (mediaEvalucionUsuario + notaForrestGump);
+            Scanner teclado = new Scanner(System.in);//instancia teclado como clase Scanner
+            double notaForrestGump = 0;
+            do {
+                System.out.println("la nota # "+ (i+1));
+                System.out.println("Ingrese nota que le darias a Forrest Gump: ");
+                try {
+                    notaForrestGump = teclado.nextDouble();
+                    if (notaForrestGump < 0 || notaForrestGump > 5) {
+                        System.out.println("solo se recibe valores para calificar entre 0 y 5");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Por favor, ingrese solo valores numericos.");
+                    teclado.next(); // Limpiar teclado
+                    notaForrestGump = 6; // Asignar un valor inválido para repetir el bucle do while
+                }
+            } while (notaForrestGump < 0 || notaForrestGump > 5);
+
+            mediaEvalucionUsuario = mediaEvalucionUsuario + notaForrestGump;
+
         }
-        System.out.println("La media de la pelicula Forrest Gump, calculada por el usuario es: " + (mediaEvalucionUsuario/3) );
+        double mediaFinal = mediaEvalucionUsuario / 3;
+        DecimalFormat formatoDosDecimales = new DecimalFormat("#.##");
+        System.out.println("La media de la pelicula Forrest Gump, calculada por el usuario es:\n " + formatoDosDecimales.format(mediaFinal) +"\n Gracias por participar");
+
     }
 }
